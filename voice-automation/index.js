1737a2559338099131d78369135b33574899dc48
Qaisar_Azeemi <qaisarazeemi@uop.edu.pk>
2021-12-23 15:59:19 +0500

    express handle added.

--- voice-automation/index.js
+++ voice-automation/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 //const { request, response } = require('express');
 const app = express();
-//app.use(express.json());
+app.use(express.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(bodyParser.json()); //this is also true and must be added to program
 port = process.env.PORT || 8080
