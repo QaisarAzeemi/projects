092f1fd910cc88acf5dfe32491f24e23a795d43c
Qaisar_Azeemi <qaisarazeemi@uop.edu.pk>
2021-12-23 13:30:46 +0500

    making voice command program

---
+++ voice_automation/index.js
@@ -0,0 +1,38 @@
+//portal CLASS_17 .... 
+const { WebhookClient } = require('dialogflow-fulfillment');
+const express = require('express');
+const bodyParser = require('body-parser');
+//const { request, response } = require('express');
+const app = express();
+//app.use(express.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json()); //this is also true and must be added to program
+port = process.env.PORT || 8080
+
+app.post("/vc", bodyParser.json(), (request, response) => {
+    const agent = new WebhookClient({ request: request, response: response })
+
+    // function calculate(agent) {
+    //     console.log(agent.request);
+    //     var num01 = agent.parameters.number01;
+    //     var num02 = agent.parameters.number02;
+    //     agent.add(`welcome to calculatorbot from backend by Qaisar, ${num01} + ${num02} = ${num01+num02}`);
+    // }
+    function turnOn() {
+        console.log(agent.request);
+        agent.add(`Turning ON the LED`);
+    }
+
+    function turnOff() {
+        console.log(agent.request);
+        agent.add(`Turning OFF the LED`);
+    }
+    let intentMap = new Map();
+    //intentMap.set('Calculate', calculate);
+    intentMap.set('Turn ON', turnOn);
+    intentMap.set('Turn OFF', turnOff);
+    agent.handleRequest(intentMap)
+})
+app.listen(port, () => {
+    console.log("Server is Up");
+})
